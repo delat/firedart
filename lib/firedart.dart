af194fb8e1ec52455608c038f85ac1f361d0e815
Pavlo Vlasenko <vlpasha84@gmail.com>
2021-02-22 23:38:18 +0200

    fix: firebase store

--- lib/firedart.dart
+++ lib/firedart.dart
@@ -4,6 +4,6 @@ export 'package:firedart/auth/client.dart';
 export 'package:firedart/auth/firebase_auth.dart';
 export 'package:firedart/auth/service_account.dart';
 export 'package:firedart/auth/token_store.dart';
-export 'package:firedart/firebase_storage/firebase_storage.dart';
+export 'package:firedart/storage/firebase_storage.dart';
 export 'package:firedart/firestore/firestore.dart';
 export 'package:firedart/firestore/models.dart';
